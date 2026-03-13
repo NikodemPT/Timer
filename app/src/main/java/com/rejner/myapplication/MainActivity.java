65f3bc17f401e292697b866c20640b17beee08e2
NikodemPT <nikodem.bak@uczen.mechaniktg.pl>
2026-03-13 12:16:04 +0100

    Guziki

--- app/src/main/java/com/rejner/myapplication/MainActivity.java
+++ app/src/main/java/com/rejner/myapplication/MainActivity.java
@@ -2,40 +2,77 @@ package com.rejner.myapplication;
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.widget.ListView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView textView;
+    private Button start;
+    private Button stop;
+    private Button reset;
+    private boolean czyAktywny;
     private int ileSekund;
-    TextView textView;
-    ListView listView;
-    boolean czyIdzie = false;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        Handler handler = new Handler();
+
         textView = findViewById(R.id.textView);
-        listView = findViewById(R.id.listView);
+        start = findViewById(R.id.start);
+        stop = findViewById(R.id.stop);
+        reset = findViewById(R.id.reset);
+
+        Handler handler = new Handler();
         handler.post(
                 new Runnable() {
                     @Override
                     public void run() {
-                        if (czyIdzie == true){
+                        if(czyAktywny) {
                             ileSekund++;
-                            textView.setText(""+ileSekund);
-                            handler.postDelayed(this, 1000);
+                            textView.setText(zwrocLadnyCzas(ileSekund));
                         }
+                        handler.postDelayed(this, 1000);
                     }
                 }
         );
+
+        start.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        czyAktywny = true;
+                    }
+                }
+        );
+        stop.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        czyAktywny = false;
+                    }
+                }
+        );
+        reset.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        czyAktywny = false;
+                        ileSekund = 0;
+                        textView.setText("00:00:00");
+                    }
+                }
+        );
+    }
+    private String zwrocLadnyCzas(int ileSekund){
+        int sekundy = ileSekund % 60;
+        int minuty = (ileSekund / 60) % 60;
+        int godziny = ileSekund / 3600;
+        return String.format("%02d:%02d:%02d", godziny, minuty, sekundy);
     }
 }
